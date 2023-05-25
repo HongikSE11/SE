0df55a30f97d1328d50e02ffd9d0ce21608d2ef7
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 04:48:52 +0900

    Update SignOut.cpp

--- SignOut.cpp
+++ SignOut.cpp
@@ -13,17 +13,17 @@ using namespace std;
 
 void SignOut::ShowUserInfo() {
 
-    vector<UserInfo> userlist;		//일반 회원 리스트 
-	vector<UserInfo> companylist;	//회사 회원 리스트
+    	vector<UserInfo> userlist;		//일반 회원 리스트 
+	vector<UserInfo> companylist;		//회사 회원 리스트
 	SignOutUI signoutui;			//boundary 클래스 인스턴스
 
 	signoutui.startinterface();		//인터페이스 시작
 	signoutui.selectCase();			//회원 ID 입력 함수
 
 	string showid=signoutui.ID;		//탈퇴하려는 회원 ID
-    UserInfo userinfo;
+    	UserInfo userinfo;
 
-    userinfo.deleteUserInfo(showid);	//회원 탈퇴
+    	userinfo.deleteUserInfo(showid);	//회원 탈퇴
 	signoutui.ShowCompleteMessage();	//완료 메세지 출력
 	
 
