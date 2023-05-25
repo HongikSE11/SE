0e634cfbb2ea510cf9f5c21b69cb87b43907291b
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 05:24:35 +0900

    Update selectMember.cpp

--- selectMember.cpp
+++ selectMember.cpp
@@ -1,9 +1,15 @@
 //헤더 선언
 #include "selectMember.h"
 #include "logIn.h"
+#include "RecuritInfo.h"
+#include "ApplyInfo.h"
 #include <string.h>
 
 void selectMember::memberInfo() 
 {
   int user_type = logIn::cheakloginInfo();  //회원 종류 판별
+  if(user_type == 1)
+    RecuritInfo::getRecuritInfoDetails();
+  else
+    ApplyInfo::getApplyInfoDetails();
 }
