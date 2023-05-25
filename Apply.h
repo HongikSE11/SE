d8e38ff087b64d8f539ec2cedcaa75362809a95f
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 04:11:31 +0900

    Update Apply.h

--- Apply.h
+++ Apply.h
@@ -9,12 +9,12 @@ private:
 	int commpany_num;		//사업자번호
 	std::string job;		//업무
 	int n_member;			//인원수
-	std::string due_date;	//마감일
+	std::string due_date;		//마감일
 
 public:
 	Apply(std::string input_name, int input_company_num, std::string input_job, int input_n_member,std::string input_due_date)
 	{
-		name = input_name;
+		name = input_name; 
 		commpany_num = input_company_num;
 		job = input_job;
 		n_member = input_n_member;
