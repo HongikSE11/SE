7bed1e419b50020f8e053fe1f00081b8b692bc42
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 04:12:02 +0900

    Update Apply.cpp

--- Apply.cpp
+++ Apply.cpp
@@ -1,3 +1,4 @@
+//헤더 선언
 #include "Apply.h"
 #include <iostream>
 #define MAX_STRING 32
@@ -7,5 +8,5 @@
 void Apply::getApplyInfo()
 {
 	FILE* out_fp = fopen(OUTPUT_FILE_NAME, "w+");
-	fprintf(out_fp, "> %s %s %d %s \n", name.c_str(), job.c_str(), n_member, due_date.c_str());
+	fprintf(out_fp, "> %s %s %d %s \n", name.c_str(), job.c_str(), n_member, due_date.c_str());	//파일 출력
 }
