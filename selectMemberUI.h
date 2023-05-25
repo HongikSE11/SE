a5fb1da9968e7a1176d576fcec139ed01518e21a
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 04:43:26 +0900

    Update selectMemberUI.h

--- selectMemberUI.h
+++ selectMemberUI.h
@@ -1,8 +1,9 @@
+//헤더 선언
 #pragma once
 class selectMemberUI
 {
 public:
-	void startInterface();
-	void showUserInfo();
+	void startInterface();	//interface 시작 함수
+	void showUserInfo();	//업무별 지원자 수, 업무별 지원 횟수 출력
 };
 
