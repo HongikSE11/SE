f37e5df9f3a953d0857bc9ceb79dd638332a9911
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 04:31:36 +0900

    Update Recurit.cpp

--- Recurit.cpp
+++ Recurit.cpp
@@ -1,29 +1,15 @@
+//í—¤ë” ì„ ì–¸
 #include "Recurit.h"
 #include <iostream>
 #include "UserInfo.h"
-//#include <tuple>
-//typedef std::tuple<std::string, int, std::string> job_information;
 
-// »ó¼ö ¼±¾ğ
+// ìƒìˆ˜ ì„ ì–¸
 #define MAX_STRING 32
 #define INPUT_FILE_NAME "input.txt"
 #define OUTPUT_FILE_NAME "output.txt"
 
 void Recurit::getRecuritInfo()
 {
-	//std::cout << job << " " << n_member << " " << due_date << std::endl;
-	
-
-	//job_information temp_info = std::make_tuple(job, n_member, due_date);
-
-	/*
-	char str[2][100];	//job, due_date ÀÔ·Â ¿ë
-	str[0] = job;
-	str[1] = due_date;
-	*/
-
-
-
 	FILE* out_fp = fopen(OUTPUT_FILE_NAME, "w+");
-	fprintf(out_fp, "> { [%s] [%d] [%s] }\n", job.c_str(), n_member, due_date.c_str()); //Ã¤¿ë Á¤º¸ ÆÄÀÏ Ãâ·Â
+	fprintf(out_fp, "> { [%s] [%d] [%s] }\n", job.c_str(), n_member, due_date.c_str()); //ì±„ìš© ì •ë³´ íŒŒì¼ ì¶œë ¥
 }
