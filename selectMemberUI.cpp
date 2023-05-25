9bebc9be4b334f90ea12d590d57be268b7c7eb32
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 04:45:04 +0900

    Update selectMemberUI.cpp

--- selectMemberUI.cpp
+++ selectMemberUI.cpp
@@ -1,11 +1,11 @@
+//헤더 선언
 #include "selectMemberUI.h"
 #include "selectMember.h"
-
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
 
-
+//상수 선언
 #define MAX_STRING 32
 #define INPUT_FILE_NAME "input.txt"
 #define OUTPUT_FILE_NAME "output.txt"
@@ -14,11 +14,12 @@
 
 void selectMemberUI::startInterface()
 {
-  // main
+  	FILE* out_fp = fopen(OUTPUT_FILE_NAME, "w+");
+	fprintf(out_fp, "지원 정보 통계");	//파일 출력
 }
 
-void selectMemberUI::showUserInfo() //...
+void selectMemberUI::showUserInfo()
 {
 	FILE* out_fp = fopen(OUTPUT_FILE_NAME, "w+");
-	fprintf(out_fp, "%s %d", job, num);
+	fprintf(out_fp, "%s %d", job, num);	//지원 정보 통계 파일 출력
 }
