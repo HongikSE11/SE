9b0271a895509ec601284d652b0d246f8b565a60
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 05:18:39 +0900

    Update RecuritInfo.h

--- RecuritInfo.h
+++ RecuritInfo.h
@@ -14,8 +14,9 @@ public:
 	{
 		num_Recurit = 0;	//생성자에서 채용 정보 수 를 0 으로 초기화
 	}
-
+	
 	static void getInfo();	//채용 정보 출력 함수
 	static void addInfo(std::string input_job, int input_n_member, std::string input_due_date); //채용 정보 입력 함수
+	static void getRecuritInfoDetails();	//업무별 지원자 수 출력
 };
 
