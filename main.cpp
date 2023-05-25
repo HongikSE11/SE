6b5d3a65c181b17c2e0223a129b51eb2349f4a93
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 04:04:09 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -24,10 +24,8 @@ int main()
 
     doTask();
 
-        return 0;
+    return 0;
 }
-
-
 
 
 
@@ -35,7 +33,7 @@ int main()
 void doTask()
 {
     // 메뉴 파싱을 위한 level 구분을 위한 변수
-    int menu_level_1 = 0, menu_level_2 = 0;
+    int menu_level_1 = 0, menu_level_2 = 0, menu_level_3 = 0;
     int is_program_exit = 0;
 
     while (!is_program_exit)
@@ -51,38 +49,110 @@ void doTask()
         {
             switch (menu_level_2)
             {
-            case 1:   // "1.1. 회원가입“ 메뉴 부분
+            case 1:   //"1.1. 회원 가입“ 메뉴 부분
             {
-                // join() 함수에서 해당 기능 수행 
-                join();
+                fscanf(in_fp, "%d", &menu_level_3);
+
+                switch (menu_level_3)
+                {
+                case 1: //"1.1.1 회사 회원 가입“ 메뉴 부분
+                {
+                    //do something
+                    break;
+                }
+                case 2: //"1.1.2 일반 회원 가입“ 메뉴 부분
+                {
+                    //do something
+                    break;
+                }
+                }
+                {
+                default:
+                    break;
+                }
 
                 break;
             }
-            case 2:
+            case 2: //"1.2 회원 탈퇴“ 메뉴 부분
             {
-
+                //do something
                 break;
             }
 
             }
-
-        case 7:
+        case 2:
+            switch (menu_level_2)
+            {
+            case 1: //"2.1 로그인“ 메뉴 부분
+            {
+                //do something
+                break;
+            }
+            case 2: //"2.2 로그아웃“ 메뉴 부분
+            {
+                //do something
+                break;
+            }
+            }
+        case 3:
+            switch (menu_level_2)
+            {
+            case 1: //"3.1 채용 정보 등록“ 메뉴 부분
+            {
+                //do something
+                break;
+            }
+            case 2: //"3.2  등록된 채용 정보 조회“ 메뉴 부분
+            {
+                //do something
+                break;
+            }
+            }
+        case 4:
+            switch (menu_level_2)
+            {
+            case 1: //"4.1 채용 정보 검색“ 메뉴 부분
+            {
+                //do something
+                break;
+            }
+            case 2: //"4.2 채용 지원“ 메뉴 부분
+            {
+                //do something
+                break;
+            }
+            case 3: //"4.3 지원 정보 조회“ 메뉴 부분
+            {
+                //do something
+                break;
+            }
+            case 4: //"4.4 지원 취소“ 메뉴 부분
+            {
+                //do something
+                break;
+            }
+            }
+        case 5:
+            switch (menu_level_2)
+            {
+            case 1: //"5.1 지원 정보 통계“ 메뉴 부분
+            {
+                //do something
+                break;
+            }
+            }
+        case 6:
         {
             switch (menu_level_2)
             {
             case 1:   // "6.1. 종료“ 메뉴 부분
             {
-
                 program_exit();
                 is_program_exit = 1;
                 break;
             }
             }
-
         }
-
-        }
-
         return;
         }
     }
@@ -96,7 +166,7 @@ void join()
      * control 및 boundary class를 이용해서 해당 기능이 구현되도록 해야 함.
      */
 
-    char user_type[MAX_STRING], name[MAX_STRING], SSN [[MAX_STRING]],
+    char user_type[MAX_STRING], name[MAX_STRING], SSN [MAX_STRING],
         address[MAX_STRING], ID[MAX_STRING], password[MAX_STRING];
 
     // 입력 형식 : 이름, 주민번호, ID, Password를 파일로부터 읽음
@@ -114,6 +184,5 @@ void join()
 
 void program_exit()
 {
-
-
+    //exit program 
 }
