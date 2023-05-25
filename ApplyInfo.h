4bd46942ea7ec543e1833ce9c3d72d5632025956
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 05:26:15 +0900

    Update ApplyInfo.h

--- ApplyInfo.h
+++ ApplyInfo.h
@@ -18,4 +18,5 @@ public:
 	static void getApplyInfo();	//지원 정보 출력 함수
 	static void addApplyInfo(std::string input_name, int input_company_num, std::string input_job,std::string due_date);	//즉시 지원 함수
 	static void cancelApply(int input_company_num);	//지원 취소 함수
+	static void getApplyDetails();	//업무별 지원 횟수 출력 함수
 };
