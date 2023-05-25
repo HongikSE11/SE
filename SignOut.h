e674b756b646c234d826addbd4c97d90665398b4
HyungDong Park <73031688+parkrokr@users.noreply.github.com>
2023-05-26 04:49:19 +0900

    Update SignOut.h

--- SignOut.h
+++ SignOut.h
@@ -1,13 +1,13 @@
 // 헤더선언
 #pragma once
 #include "UserInfo.h"
+
 class SignOut
 {
 public:
 	void ShowUserInfo(); //회원 탈퇴하는 회원 정보 출력 함수
 	
 	static vector<UserInfo> userlist;	//일반 회원 리스트
-	static vector<UserInfo> companylist; //회사 회원 리스트
-
+	static vector<UserInfo> companylist; 	//회사 회원 리스트
 };
 
