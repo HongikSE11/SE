be4ce0fbb5e1e9a62b70a73c47fd9ddd55a8d7ac
PARKJEONGJOON <99483101+PARKJEONGJOON@users.noreply.github.com>
2023-05-26 04:57:48 +0900

    Update logoutUI.cpp

--- logoutUI.cpp
+++ logoutUI.cpp
@@ -2,19 +2,17 @@
 
 using namespace std;
 void logoutUI::startinterface() {
-	cout << "로그아웃 시작\n";
-
+	FILE* out_fp = fopen(OUTPUT_FILE_NAME, "w+");
+	fprintf(out_fp, "2.2. 로그아웃\n")
 
 }
 
 void logoutUI::ShowCompleteMessage() {
-	cout << " 로그아웃 완료\n";
+	
 	FILE* out_fp = fopen(OUTPUT_FILE_NAME, "w+");
-	fprintf(out_fp, "2.2. 로그아웃\n")
 	fprintf(out_fp, "%s , ID);
 }
 void logoutUI::inputinfo() {
-	cout << "ID를 입력하시오 .\n";
 	
 	FILE* in_fp = fopen(INPUT_FILE_NAME, "r+");
 	fscanf(in_fp, "%s , ID); 		
